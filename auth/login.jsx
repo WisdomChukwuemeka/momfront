4d6bc85328c99f7d87eae6f206b1d36ab2e3d45f
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-08-17 19:52:21 +0100

    second c

--- auth/login.jsx
+++ auth/login.jsx
@@ -1,6 +1,7 @@
 import { useNavigate, Link } from "react-router-dom";
 import { AuthAPI } from "../components/services/api";
 import { useState, useEffect } from "react";
+
 export const Login = ({ onLogin }) => {
   const navigate = useNavigate();
   const [credentials, setcredentials] = useState({
@@ -12,17 +13,12 @@ export const Login = ({ onLogin }) => {
   const [success, setSuccess] = useState("");
   const [loading, setLoading] = useState(false);
 
-  
-      
-        useEffect(() => {
-          // ✅ Simulate loading time (e.g. data fetch)
-          const timer = setTimeout(() => {
-            setLoading(false);
-          }, 3000); // 2 seconds spinner
-      
-          return () => clearTimeout(timer);
-        }, []);
-      
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleChange = (e) => {
     setcredentials((prevData) => ({
@@ -48,18 +44,17 @@ export const Login = ({ onLogin }) => {
       setTimeout(() => setSuccess(""), 2000);
       const is_superuser = JSON.parse(localStorage.getItem("is_superuser"));
       if (is_superuser) {
-        navigate("/", { replace: true }); // 🔹 replace prevents going back to login
+        navigate("/", { replace: true });
       }
-    // ✅ Notify Navbar immediately
-    window.dispatchEvent(new Event("authChange"));
-
+      window.dispatchEvent(new Event("authChange"));
       onLogin(response.data.user);
     } catch (error) {
       setError(
-        error.response?.data?.email?.[0] || error.response?.data?.password?.[0] ||
-        error.response?.data?.non_field_errors?.[0] ||
-        error.response?.data?.detail ||
-        "An unexpected error occurred. Please try again."
+        error.response?.data?.email?.[0] ||
+          error.response?.data?.password?.[0] ||
+          error.response?.data?.non_field_errors?.[0] ||
+          error.response?.data?.detail ||
+          "An unexpected error occurred. Please try again."
       );
       setTimeout(() => setError(""), 3000);
     } finally {
@@ -69,177 +64,92 @@ export const Login = ({ onLogin }) => {
 
   return (
     <>
-     <div className="min-h-screen flex justify-center items-center p-8 md:p-30 container mx-auto ">
+      <div className="min-h-screen flex justify-center items-center p-8 md:p-30 container mx-auto ">
         <div className="">
-           <div>
-            <h2 className="text-3xl font-bold text-center mb-6">Welcome Admin</h2>
+          <div>
+            <h2 className="text-3xl font-bold text-center mb-6">
+              Welcome Admin
+            </h2>
             {success && (
-            <div className="text-green-600 text-center">{success}</div>
-          )}
-          {error && (
-            <div className="text-red-600 text-sm text-center">{error}</div>
-          )}
+              <div className="text-green-600 text-center">{success}</div>
+            )}
+            {error && (
+              <div className="text-red-600 text-sm text-center">{error}</div>
+            )}
 
-          <form onSubmit={handleSubmit} className="space-y-6">
-            {/* Email */}
-            <div>
-               <label
-                htmlFor="email"
-                className="block text-sm font-medium text-black"
-              >
-                Email Address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={credentials.email}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full px-4 py-3 md:py-4 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-                placeholder="you@example.com"
-              />
-            </div>
+            <form onSubmit={handleSubmit} className="space-y-6">
+              {/* Email */}
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-black"
+                >
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={credentials.email}
+                  onChange={handleChange}
+                  required
+                  autoComplete="email"
+                  className="mt-1 block w-full px-4 py-3 md:py-4 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="you@example.com"
+                />
+              </div>
 
-            {/* {Password} */}
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-black"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={credentials.password}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full px-4 py-3 
+              {/* Password */}
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-black"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={credentials.password}
+                  onChange={handleChange}
+                  required
+                  autoComplete="current-password"
+                  className="mt-1 block w-full px-4 py-3 
                 md:py-4 md:text-2xl border border-gray-300 
                 rounded-md shadow-sm focus:ring-blue-500 
                 focus:border-blue-500"
-                placeholder="••••••••"
-              />
-            </div>
+                  placeholder="••••••••"
+                />
+              </div>
 
-            {/* Submit Button */}
-            <button
-              type="submit"
-              disabled={loading}
-              className={`w-full px-4 py-3 md:py-4 md:text-3xl flex justify-center items-center gap-2 
+              {/* Submit Button */}
+              <button
+                type="submit"
+                disabled={loading}
+                className={`w-full px-4 py-3 md:py-4 md:text-3xl flex justify-center items-center gap-2 
     ${
       loading
         ? "bg-green-400 cursor-not-allowed"
         : "bg-blue-800 hover:bg-blue-700"
     }
     rounded-md text-white font-semibold transition-colors`}
-            >
-              {loading && <i className="bi bi-arrow-repeat animate-spin"></i>}
-              {loading ? "Logging in..." : "Login"}
-            </button>
-          </form>
-            
-                 {/* Extra link */}
-           <p className="text-center text-sm md:text-2xl mt-6 text-gray/70">
-             Don't have an account?{" "}
-             <Link to="/register" class="text-blue-300">
-               Sign up
-            </Link>
-          </p>
+              >
+                {loading && <i className="bi bi-arrow-repeat animate-spin"></i>}
+                {loading ? "Logging in..." : "Login"}
+              </button>
+            </form>
 
-           </div>
+            {/* Extra link */}
+            <p className="text-center text-sm md:text-2xl mt-6 text-gray/70">
+              Don't have an account?{" "}
+              <Link to="/register" className="text-blue-300">
+                Sign up
+              </Link>
+            </p>
+          </div>
         </div>
-     </div>
+      </div>
     </>
   );
 };
-
-
-
-
-
-//  <div className="min-h-screen  lg:px-[40%] flex items-center justify-center bg-gradient-to-br">
-//         <div className="w-full h-fit  p-8 px-10 rounded-3xl borde bg-blue-700 shadow-lg">
-//           <div class="flex flex-col">
-//             <i className="bi bi-circle-half text-2xl md:text-4xl mb-3 font-bold text-center"></i>
-//             <h2 className="text-2xl md:text-3xl font-bold mb-6 text-center">
-//               Swapskill
-//             </h2>
-//           </div>
-
-//           {success && (
-//             <div className="text-green-600 text-center">{success}</div>
-//           )}
-//           {error && (
-//             <div className="text-red-600 text-sm text-center">{error}</div>
-//           )}
-
-//           <form onSubmit={handleSubmit} className="space-y-6">
-//             {/* Email */}
-//             <div>
-//               <label
-//                 htmlFor="email"
-//                 className="block text-sm font-medium text-white"
-//               >
-//                 Email Address
-//               </label>
-//               <input
-//                 type="email"
-//                 name="email"
-//                 id="email"
-//                 value={credentials.email}
-//                 onChange={handleChange}
-//                 required
-//                 className="mt-1 block w-full px-4 py-3 md:py-2 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-//                 placeholder="you@example.com"
-//               />
-//             </div>
-
-//             {/* {Password} */}
-//             <div>
-//               <label
-//                 htmlFor="password"
-//                 className="block text-sm font-medium text-white"
-//               >
-//                 Password
-//               </label>
-//               <input
-//                 type="password"
-//                 name="password"
-//                 id="password"
-//                 value={credentials.password}
-//                 onChange={handleChange}
-//                 required
-//                 className="mt-1 block w-20 md-w-full px-4 py-3 md:py-2 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-//                 placeholder="••••••••"
-//               />
-//             </div>
-
-//             {/* Submit Button */}
-//             <button
-//               type="submit"
-//               disabled={loading}
-//               className={`w-full px-4 py-3 md:py-2 md:text-2xl flex justify-center items-center gap-2 
-//     ${
-//       loading
-//         ? "bg-green-400 cursor-not-allowed"
-//         : "bg-green-600 hover:bg-green-700"
-//     }
-//     rounded-md text-white font-semibold transition-colors`}
-//             >
-//               {loading && <i className="bi bi-arrow-repeat animate-spin"></i>}
-//               {loading ? "Logging in..." : "Login"}
-//             </button>
-//           </form>
-
-//           {/* Extra link */}
-//           <p className="text-center text-sm md:text-2xl mt-6 text-white/70">
-//             Don't have an account?{" "}
-//             <Link to="/registration" class="text-green-300">
-//               Sign up
-//             </Link>
-//           </p>
-//         </div>
-//       </div>
--- auth/register.jsx
+++ auth/register.jsx
@@ -1,6 +1,7 @@
 import { useNavigate, Link } from "react-router-dom";
 import { AuthAPI } from "../components/services/api";
 import { useState, useEffect } from "react";
+
 export const Register = ({ onRegister }) => {
   const navigate = useNavigate();
   const [credentials, setcredentials] = useState({
@@ -13,25 +14,21 @@ export const Register = ({ onRegister }) => {
   const [success, setSuccess] = useState("");
   const [loading, setLoading] = useState(false);
 
-  
-      
-        useEffect(() => {
-          // ✅ Simulate loading time (e.g. data fetch)
-          const timer = setTimeout(() => {
-            setLoading(false);
-          }, 2000); // 2 seconds spinner
-      
-          return () => clearTimeout(timer);
-        }, []);
-      
-         if (loading) {
-          return (
-            <div className="flex flex-col items-center justify-center h-screen">
-              <div className="animate-spin rounded-full h-20 w-20 border-t-4 border-b-4 border-green-500"></div>
-              <p className="mt-4 text-white">Loading...</p>
-            </div>
-          );
-        }
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="animate-spin rounded-full h-20 w-20 border-t-4 border-b-4 border-green-500"></div>
+        <p className="mt-4 text-white">Loading...</p>
+      </div>
+    );
+  }
 
   const handleChange = (e) => {
     setcredentials((prevData) => ({
@@ -59,14 +56,14 @@ export const Register = ({ onRegister }) => {
       if (is_superuser) {
         navigate("/login");
       }
-
       onRegister(response.data.user);
     } catch (error) {
       setError(
-        error.response?.data?.email?.[0] || error.response?.data?.password?.[0] ||
-        error.response?.data?.confirm_password?.[0] ||
-        error.response?.data?.non_field_errors?.[0] ||
-        "An unexpected error occurred. Please try again."
+        error.response?.data?.email?.[0] ||
+          error.response?.data?.password?.[0] ||
+          error.response?.data?.confirm_password?.[0] ||
+          error.response?.data?.non_field_errors?.[0] ||
+          "An unexpected error occurred. Please try again."
       );
       setTimeout(() => setError(""), 3000);
     } finally {
@@ -76,194 +73,108 @@ export const Register = ({ onRegister }) => {
 
   return (
     <>
-     <div className="min-h-screen flex justify-center items-center p-8 md:p-30 container mx-auto ">
+      <div className="min-h-screen flex justify-center items-center p-8 md:p-30 container mx-auto ">
         <div className="">
-           <div>
+          <div>
             <h2 className="text-3xl font-bold text-center mb-6">Welcome Admin</h2>
             {success && (
-            <div className="text-green-600 text-center">{success}</div>
-          )}
-          {error && (
-            <div className="text-red-600 text-sm text-center">{error}</div>
-          )}
+              <div className="text-green-600 text-center">{success}</div>
+            )}
+            {error && (
+              <div className="text-red-600 text-sm text-center">{error}</div>
+            )}
 
-          <form onSubmit={handleSubmit} className="space-y-6">
-            {/* Email */}
-            <div>
-               <label
-                htmlFor="email"
-                className="block text-sm font-medium text-black"
-              >
-                Email Address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={credentials.email}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full px-4 py-3 md:py-4 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-                placeholder="you@example.com"
-              />
-            </div>
+            <form onSubmit={handleSubmit} className="space-y-6">
+              {/* Email */}
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-black"
+                >
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={credentials.email}
+                  onChange={handleChange}
+                  required
+                  autoComplete="email"
+                  className="mt-1 block w-full px-4 py-3 md:py-4 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="you@example.com"
+                />
+              </div>
 
-            {/* {Password} */}
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-black"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={credentials.password}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full px-4 py-3 md:py-4 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-                placeholder="••••••••"
-              />
-            </div>
+              {/* Password */}
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-black"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={credentials.password}
+                  onChange={handleChange}
+                  required
+                  autoComplete="new-password"
+                  className="mt-1 block w-full px-4 py-3 md:py-4 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="••••••••"
+                />
+              </div>
 
-            {/* Confirm Password */}
-            <div>
-              <label
-                htmlFor="confirm_password"
-                className="block text-sm font-medium text-black"
-              >
-                Confirm Password
-              </label>
-              <input
-                type="password"
-                name="confirm_password"
-                id="confirm_password"
-                value={credentials.confirm_password}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full px-4 py-3 md:py-4 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-                placeholder="••••••••"
-              />  
-            </div>
+              {/* Confirm Password */}
+              <div>
+                <label
+                  htmlFor="confirm_password"
+                  className="block text-sm font-medium text-black"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  name="confirm_password"
+                  id="confirm_password"
+                  value={credentials.confirm_password}
+                  onChange={handleChange}
+                  required
+                  autoComplete="new-password"
+                  className="mt-1 block w-full px-4 py-3 md:py-4 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="••••••••"
+                />
+              </div>
 
-            {/* Submit Button */}
-            <button
-              type="submit"
-              disabled={loading}
-              className={`w-full px-4 py-3 md:py-4 md:text-3xl flex justify-center items-center gap-2 
+              {/* Submit Button */}
+              <button
+                type="submit"
+                disabled={loading}
+                className={`w-full px-4 py-3 md:py-4 md:text-3xl flex justify-center items-center gap-2 
     ${
       loading
         ? "bg-green-400 cursor-not-allowed"
         : "bg-blue-800 hover:bg-blue-700"
     }
     rounded-md text-white font-semibold transition-colors`}
-            >
-              {loading && <i className="bi bi-arrow-repeat animate-spin"></i>}
-              {loading ? "Creating..." : "Register"}
-            </button>
-          </form>
-            
-                 {/* Extra link */}
-           <p className="text-center text-sm md:text-2xl mt-6 text-gray/70">
-             Already have an account?{" "}
-             <Link to="/login" class="text-blue-300">
-               Login
-            </Link>
-          </p>
+              >
+                {loading && <i className="bi bi-arrow-repeat animate-spin"></i>}
+                {loading ? "Creating..." : "Register"}
+              </button>
+            </form>
 
-           </div>
+            {/* Extra link */}
+            <p className="text-center text-sm md:text-2xl mt-6 text-gray/70">
+              Already have an account?{" "}
+              <Link to="/login" className="text-blue-300">
+                Login
+              </Link>
+            </p>
+          </div>
         </div>
-     </div>
+      </div>
     </>
   );
 };
-
-
-
-
-
-//  <div className="min-h-screen  lg:px-[40%] flex items-center justify-center bg-gradient-to-br">
-//         <div className="w-full h-fit  p-8 px-10 rounded-3xl borde bg-blue-700 shadow-lg">
-//           <div class="flex flex-col">
-//             <i className="bi bi-circle-half text-2xl md:text-4xl mb-3 font-bold text-center"></i>
-//             <h2 className="text-2xl md:text-3xl font-bold mb-6 text-center">
-//               Swapskill
-//             </h2>
-//           </div>
-
-//           {success && (
-//             <div className="text-green-600 text-center">{success}</div>
-//           )}
-//           {error && (
-//             <div className="text-red-600 text-sm text-center">{error}</div>
-//           )}
-
-//           <form onSubmit={handleSubmit} className="space-y-6">
-//             {/* Email */}
-//             <div>
-//               <label
-//                 htmlFor="email"
-//                 className="block text-sm font-medium text-white"
-//               >
-//                 Email Address
-//               </label>
-//               <input
-//                 type="email"
-//                 name="email"
-//                 id="email"
-//                 value={credentials.email}
-//                 onChange={handleChange}
-//                 required
-//                 className="mt-1 block w-full px-4 py-3 md:py-2 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-//                 placeholder="you@example.com"
-//               />
-//             </div>
-
-//             {/* {Password} */}
-//             <div>
-//               <label
-//                 htmlFor="password"
-//                 className="block text-sm font-medium text-white"
-//               >
-//                 Password
-//               </label>
-//               <input
-//                 type="password"
-//                 name="password"
-//                 id="password"
-//                 value={credentials.password}
-//                 onChange={handleChange}
-//                 required
-//                 className="mt-1 block w-20 md-w-full px-4 py-3 md:py-2 md:text-2xl border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-//                 placeholder="••••••••"
-//               />
-//             </div>
-
-//             {/* Submit Button */}
-//             <button
-//               type="submit"
-//               disabled={loading}
-//               className={`w-full px-4 py-3 md:py-2 md:text-2xl flex justify-center items-center gap-2 
-//     ${
-//       loading
-//         ? "bg-green-400 cursor-not-allowed"
-//         : "bg-green-600 hover:bg-green-700"
-//     }
-//     rounded-md text-white font-semibold transition-colors`}
-//             >
-//               {loading && <i className="bi bi-arrow-repeat animate-spin"></i>}
-//               {loading ? "Logging in..." : "Login"}
-//             </button>
-//           </form>
-
-//           {/* Extra link */}
-//           <p className="text-center text-sm md:text-2xl mt-6 text-white/70">
-//             Don't have an account?{" "}
-//             <Link to="/registration" class="text-green-300">
-//               Sign up
-//             </Link>
-//           </p>
-//         </div>
-//       </div>
